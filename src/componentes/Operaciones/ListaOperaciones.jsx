652295a660c7a578f79eb675c41b96805d17de61
Edison R. <edison@correo.com>
2025-09-22 15:07:28 -0500

    Quitar comentarios

--- src/componentes/Operaciones/ListaOperaciones.jsx
+++ src/componentes/Operaciones/ListaOperaciones.jsx
@@ -1,73 +1,6 @@
-// import { useState, useEffect } from 'react'
-
 import estilos from "./ListaOperaciones.module.css";
-// import { obtenerOperaciones } from '../../servicios/OperacionesServices';
 
 function ListaOperaciones({ operaciones, loading, onEditar, onEliminar }) {
-
-  // const [operaciones, setOperaciones] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // Datos de ejemplo - se debe reemplazar con la llamada a la API
-  /*   
-    const operacionesEjemplo = [
-      {
-        operacionID: 1,
-        identificacion: '1718564883',
-        nombre: 'JOSE RODRIGUEZ',
-        tipoCredito: '002',
-        tipoCreditoNombre: 'Crédito de Consumo',
-        monto: 55660.00,
-        fechaInicio: '2023-06-23',
-        plazoMeses: 208,
-        aprobado: true
-      },
-      {
-        operacionID: 2,
-        identificacion: '1040735005',
-        nombre: 'GONZALO MARTINEZ',
-        tipoCredito: '002',
-        tipoCreditoNombre: 'Crédito de Consumo',
-        monto: 855205.33,
-        fechaInicio: '2022-10-31',
-        plazoMeses: 360,
-        aprobado: false
-      },
-      {
-        operacionID: 3,
-        identificacion: '411520204',
-        nombre: 'DONATILA GARCIA',
-        tipoCredito: '002',
-        tipoCreditoNombre: 'Crédito de Consumo',
-        monto: 72800.00,
-        fechaInicio: '2023-04-25',
-        plazoMeses: 263,
-        aprobado: true
-      }
-    ];
-  
-   */
-
-  /*
-  useEffect(() => {
-    // Simular carga de datos
-         
-        // setTimeout(() => {
-        //   setOperaciones(operacionesEjemplo);
-        //   setLoading(false);
-        // }, 1000);
-     
-    var consultarOperaciones = async () => {
-      var datosOperaciones = await obtenerOperaciones();
-      setOperaciones(datosOperaciones);
-      setLoading(false);
-      return datosOperaciones;
-    }
-    consultarOperaciones();
-    
-
-  }, []);
-*/
 
   const formatoDinero = (cantidad) => {
     return new Intl.NumberFormat('es-EC', {
@@ -76,12 +9,9 @@ function ListaOperaciones({ operaciones, loading, onEditar, onEliminar }) {
     }).format(cantidad);
   };
 
-
-
   const formatoFecha = (fecha) => {
     return new Date(fecha).toLocaleDateString('es-EC');
   };
-
 
   const calcularFechaFin = (fechaInicio, plazoMeses) => {
     const fecha = new Date(fechaInicio);
